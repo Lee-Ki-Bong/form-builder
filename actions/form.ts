98cfe621f6d61932424a95c1dcdc333c7662e4ac
dhkehd2 <dhkehd2@gmail.com>
2025-02-11 14:53:02 +0900

    chore: Update package-lock.json and next.config.ts, add GetFormStats to dashboard page

---
+++ actions/form.ts
@@ -0,0 +1,40 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+import { currentUser } from "@clerk/nextjs/server"
+
+class UserNotFoundError extends Error {
+  constructor() {
+    super("User not found")
+  }
+}
+
+// 사용자의 폼 통계를 가져오는 함수
+export async function GetFormStats() {
+  const user = await currentUser()
+  if (!user) {
+    throw new UserNotFoundError() // 사용자를 찾을 수 없을 때 에러 발생
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id
+    },
+    _sum: {
+      visits: true,
+      submissions: true
+    }
+  })
+
+  const visits = stats._sum.visits || 0 // 방문 수
+  const submissions = stats._sum.submissions || 0 // 제출 수
+  let submissionRate = visits > 0 ? (submissions / visits) * 100 : 0 // 제출 비율
+  const bounceRate = 100 - submissionRate // 이탈률
+
+  return {
+    visits,
+    submissions,
+    submissionRate,
+    bounceRate
+  }
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,10 @@
+import { GetFormStats } from "@/actions/form"
+
 export default function Home() {
   return <div>Hello</div>
+}
+
+function CardStatesWrapper() {
+  const stats = GetFormStats()
+  return <StatsCard loading={false} data={stats} />
 }
