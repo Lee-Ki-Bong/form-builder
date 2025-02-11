3d5f6c82a88894ea162c540d1ccdb500a39a9678
dhkehd2 <dhkehd2@gmail.com>
2025-02-11 16:11:02 +0900

    chore: Update dashboard page to display form statistics

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,78 @@
 import { GetFormStats } from "@/actions/form"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { ReactNode, Suspense } from "react"
+import { LuView } from "react-icons/lu"
+import { use } from "react"
 
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div className="container pt-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatesWrapper />
+      </Suspense>
+    </div>
+  )
 }
 
-function CardStatesWrapper() {
-  const stats = GetFormStats()
-  return <StatsCard loading={false} data={stats} />
+async function CardStatesWrapper() {
+  const stats = await GetFormStats()
+  return <StatsCards loading={false} data={stats} />
+}
+
+interface StatsCardProps {
+  loading: boolean
+  data?: Awaited<ReturnType<typeof GetFormStats>>
+}
+
+function StatsCards(props: StatsCardProps) {
+  const { loading, data } = props
+  return (
+    <div className="grid w-full grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        icon={<LuView className="text-blue-600" />}
+        helporText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-blue-600"
+      />
+    </div>
+  )
+}
+
+function StatsCard({
+  title,
+  icon,
+  helporText,
+  value,
+  loading,
+  className
+}: {
+  title: string
+  icon: ReactNode
+  helporText: string
+  value: string
+  loading: boolean
+  className?: string
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle>{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading ?? (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="pt-1 text-xs text-muted-foreground">{helporText}</p>
+      </CardContent>
+    </Card>
+  )
 }
